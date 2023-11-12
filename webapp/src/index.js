a0effb888dd559df123eda2c049fe55682ca6dfb
Noah Haines <Noahhaines171@gmail.com>
2023-11-12 15:53:16 -0500

    add eslint

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-  crossorigin="anonymous"
+  crossOrigin="anonymous"
 />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
